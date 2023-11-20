1254115fcfe3fb87d57feebb703ee0d1fa37562b
UmbertoSerino <serumberto@gmail.com>
2023-11-20 16:41:28 +0100

    added function random and user required

--- js/script.js
+++ js/script.js
@@ -1,8 +1,28 @@
 // Pari e Dispari:
 // L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
 
+const oddOrEven = prompt("Pari o dispari?");
+const userNumber = parseInt(prompt("Inserisci un numero da 1 a 5"));
+console.log("Scelta utente: ", oddOrEven);
+console.log("Numero scelto: ", userNumber);
 
 // Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// ========== Funzioni ==========
+function pcRandomGen(){
+    Math.floor(Math.random () * 5) +1;
+}
+
+const pcNumber = pcRandomGen();
+console.log("Scelta computer:" + pcNumber);
+
+
+
+const result = userNumber + pcNumber;
+if (userNumber + pcRandom % 0 ) {
+    console.log('è DISPARI')    
+} else {
+    console.log("è DISPARI")
+}
 
 // Sommiamo i due numeri
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando un'altra funzione)
@@ -11,10 +31,3 @@
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
 // E' vietato usare split(), reverse() e simili.
-
-// Pari e Dispari:
-// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
-const oddOrEven = prompt("Pari o dispari?");
-const userNumber = parseInt(prompt("Inserisci un numero da 1 a 5"));
-console.log("Scelta utente: ", oddOrEven);
-console.log("Numero scelto: ", userNumber);
