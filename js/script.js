f21654334e7ad2227ebb02017e8035a65fb1f0ff
UmbertoSerino <serumberto@gmail.com>
2023-11-20 18:12:01 +0100

    complete odd or even

--- js/script.js
+++ js/script.js
@@ -1,33 +1,34 @@
-// Pari e Dispari:
-// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
-
-const oddOrEven = prompt("Pari o dispari?");
+// Pari e Dispari
+const requiredOddOrEven = prompt("Pari o dispari?");
 const userNumber = parseInt(prompt("Inserisci un numero da 1 a 5"));
-console.log("Scelta utente: ", oddOrEven);
+console.log("Scelta utente: ", requiredOddOrEven);
 console.log("Numero scelto: ", userNumber);
 
-// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
-// ========== Funzioni ==========
-function pcRandomGen(){
-    Math.floor(Math.random () * 5) +1;
-}
 
 const pcNumber = pcRandomGen();
-console.log("Scelta computer:" + pcNumber);
-
-
-
-const result = userNumber + pcNumber;
-if (userNumber + pcRandom % 0 ) {
-    console.log('è DISPARI')    
-} else {
-    console.log("è DISPARI")
-}
+console.log("Scelta computer: " + pcNumber);
 
 // Sommiamo i due numeri
-// Stabiliamo se la somma dei due numeri è pari o dispari (usando un'altra funzione)
+const result = pcNumber + userNumber;
+
+const resultOddOrEven = oddOrEven(result);
+console.log("Risultato: " + resultOddOrEven);
+
 // Dichiariamo chi ha vinto.
-// Palidroma:
-// Chiedere all’utente di inserire una parola
-// Creare una funzione per capire se la parola inserita è palindroma
-// E' vietato usare split(), reverse() e simili.
+if (requiredOddOrEven === resultOddOrEven) {
+    console.log("Hai vinto!");
+} else {
+    console.log("Ha vinto il computer!");
+}
+
+
+// ========== Funzioni ==========
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+function pcRandomGen(){
+    return Math.floor(Math.random () * 5) + 1;
+}
+
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando un'altra funzione)
+function oddOrEven(result) {
+    return result % 2 === 0 ? "Pari" : "Dispari";
+}
